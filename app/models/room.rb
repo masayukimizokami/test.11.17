85087055a07ff49061daa86f6ba680b8c1b0c225
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2021-12-14 11:50:20 +0900

    バリデート変更

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,7 @@ class Room < ApplicationRecord
     has_many :reserves #予約データ
 
     validates :room_name, presence: true
-    validates :room_image, presence: true
+    validates :image, presence: true
     validates :room_price, presence: true,numericality: true
     validates :room_PR, presence: true ,length: { in: 1..50 }
     validates :room_address, presence: true
