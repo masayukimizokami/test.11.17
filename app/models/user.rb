4aefb5aecf3e6d0475ab7b6ec0d94ea758c2980b
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2021-12-15 06:16:39 +0900

    user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         has_one_attached :image
+
          validates :name, presence: true
          validates :email, uniqueness: true, presence: true
 end
